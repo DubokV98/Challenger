5a29b29857bea7c288ec5855073509f18778611c
DubokV <Uladzislau.Dubok@itechart-group.com>
2022-09-29 14:40:12 +0200

    T-2 Add common module to project Prepare structure of projects add dependencies to poms create Main Class for shop service Add checkStyle checker

---
+++ shop-service/src/main/java/org/challenger/shopservice/ShopServiceApplication.java
@@ -0,0 +1,20 @@
+package org.challenger.shopservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author u.dubok
+ * @since 9/29/2022
+ */
+@SpringBootApplication
+public class ShopServiceApplication {
+    /**
+     * Application entry point.
+     *
+     * @param args passed to app
+     */
+    public static void main(final String[] args) {
+        SpringApplication.run(ShopServiceApplication.class, args);
+    }
+}
---
+++ shop-service/src/main/java/org/challenger/shopservice/config/ScanConfig.java
@@ -0,0 +1,11 @@
+package org.challenger.shopservice.config;
+
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author u.dubok
+ * @since 9/29/2022
+ */
+@ComponentScan(basePackages = {"org.challenger.shopservice"})
+public class ScanConfig {
+}
