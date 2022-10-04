3f6312c46b252401d395a61c038fca76f72e6b71
DubokV <Uladzislau.Dubok@itechart-group.com>
2022-10-04 09:50:46 +0200

    T-3 Add mapper add dependency add application.yml to shop-service

---
+++ common/src/main/java/org/challenger/common/mapper/MappedDto.java
@@ -0,0 +1,11 @@
+package org.challenger.common.mapper;
+
+/**
+ * Special marker, used for type management in the {@link Mapper}.
+ *
+ * @author u.dubok
+ * @see Mapper
+ * @since 10.04.2021
+ */
+public interface MappedDto {
+}
